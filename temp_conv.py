c6327fccb7232a2646cbcf37c0c256a0f814ba86
Harold Wegner <hwegner1@ivytech.edu>
2026-09-16 12:38:25 -0400

    added fonts and styling

--- temp_conv.py
+++ temp_conv.py
@@ -2,6 +2,7 @@ from logging import root
 import tkinter as tk
 from tkinter import ttk
 from tkinter import *
+from tkinter import font
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
@@ -55,7 +56,7 @@ class Main(tk.Tk):
         super().__init__()
         self.geometry("900x600")
         self.title("Temperature Converter")
-
+        self.configure(bg = 'grey')
         notebook = ttk.Notebook(self)
         notebook.pack(expand=True, fill='both')
 
@@ -68,23 +69,23 @@ class Main(tk.Tk):
 
         conv = Conversions()
         
-        
-        ttk.Label(frame1, text="Value:").grid(row=0, column=0, padx=(0, 8), sticky="w")
+        basic_font = font.Font(family ="Times New Roman", size=12)
+        ttk.Label(frame1, text="Value:",font=basic_font).grid(row=0, column=0, padx=(0, 8), sticky="w")
         value_entry = ttk.Entry(frame1, width=12)
         value_entry.insert(0, "100")
         value_entry.grid(row=0, column=1, padx=(0, 14), sticky="w")
 
-        ttk.Label(frame1, text="From:").grid(row=0, column=2, padx=(0, 6), sticky="w")
+        ttk.Label(frame1, text="From:", font=basic_font).grid(row=0, column=2, padx=(0, 6), sticky="w")
         from_var = tk.StringVar(value="Celsius")
         from_menu = ttk.OptionMenu(frame1, from_var, from_var.get(), *temperature_options)
         from_menu.grid(row=0, column=3, padx=(0, 14), sticky="w")
 
-        ttk.Label(frame1, text="To:").grid(row=0, column=4, padx=(0, 6), sticky="w")
+        ttk.Label(frame1, text="To:", font=basic_font).grid(row=0, column=4, padx=(0, 6), sticky="w")
         to_var = tk.StringVar(value="Kelvin")
         to_menu = ttk.OptionMenu(frame1, to_var, to_var.get(), *temperature_options)
         to_menu.grid(row=0, column=5, padx=(0, 14), sticky="w")
         
-        convert_button = ttk.Button(frame1,text="Convert", command=lambda: result_label.config(text=f"Result: {round(conv.convert_temperature(float(value_entry.get()), from_var.get(), to_var.get()), 2)} {to_var.get()}"))
+        convert_button = ttk.Button(frame1,text="Convert", command=lambda: result_label.config(text=f"Result: {round(convert_temperature(float(value_entry.get()), from_var.get(), to_var.get()), 2)} {to_var.get()}"))
         convert_button.grid(row=0, column=6, sticky="w")
         
         result_label = tk.Label(frame1, text="Result will appear here", bg="#eef7ff", relief="flat", anchor="w")
